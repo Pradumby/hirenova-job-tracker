567790fd7ff178b6690e3cc72087f1eee86980ef
Pradumby <anupy08115@gmail.com>
2026-03-20 21:01:38 +0530

    feat(auth-ui): create login page UI

--- hirenova-frontend/src/pages/Login.jsx
+++ hirenova-frontend/src/pages/Login.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../features/auth/authSlice";
+
 const Login = () => {
-  return <div>Login Page</div>;
+  const dispatch = useDispatch();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(form));
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form onSubmit={handleSubmit} className="p-6 shadow-lg rounded w-80">
+        <h2 className="text-xl mb-4">Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full mb-3 p-2 border"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full mb-3 p-2 border"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+
+        <button className="bg-blue-500 text-white w-full p-2">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
